fc9bab2aa8cc23206a0b388f4f20363c26535704
alexandra.kerezsi.18@gmail.com <alexandra.kerezsi.18@gmail.com>
2023-01-23 18:38:32 +0200

    adaugat verificari

--- Lab5ex1/Program.cs
+++ Lab5ex1/Program.cs
@@ -11,8 +11,20 @@ namespace Lab5ex1
             Console.WriteLine("Introduceti primul string = ");
             string string1 = Console.ReadLine();
 
+            if (string.IsNullOrEmpty(string1) || string.IsNullOrWhiteSpace(string1))
+            {
+                Console.WriteLine("Nu ai introdus nimic, sau ai introdus spatiu gol. Ruleaza din nou programul.");
+                return;
+            }
+
             Console.WriteLine("Introduceti al doilea string = ");
             string string2 = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(string2) || string.IsNullOrWhiteSpace(string2))
+            {
+                Console.WriteLine("Nu ai introdus nimic, sau ai introdus spatiu gol. Ruleaza din nou programul.");
+                return;
+            }
 
             /* Curs - Slide 26 => string.IndexOf: intoarce primul index in cadrul string-ului unde se regaseste (sau unde incepe) textul specificat ca si parametru */
             int pozitie = string1.IndexOf(string2);
